fce1d0b86cd0916020508e3ab60932e7fe3c038c
boracles <100pour100@naver.com>
2021-10-26 03:33:56 +0900

    녹음 버튼 고정

--- Assets/02.Scripts/SoundLampButtonCtrl.cs
+++ Assets/02.Scripts/SoundLampButtonCtrl.cs
@@ -13,6 +13,9 @@ public class SoundLampButtonCtrl : MonoBehaviour
     public AudioClip buttonOff;
 
     public GameObject buttonCollider;
+    private ConfigurableJoint joint;
+    public SoftJointLimit softJointLimit;
+    public float limit;
 
     public bool onButton;
 
@@ -20,6 +23,8 @@ public class SoundLampButtonCtrl : MonoBehaviour
     {
         audio = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
+        joint = GetComponent<ConfigurableJoint>();
+        softJointLimit = new SoftJointLimit();
     }
 
     void Start()
@@ -34,34 +39,42 @@ public class SoundLampButtonCtrl : MonoBehaviour
 
     void OnTriggerEnter(Collider coll)
     {
-        // 버튼 콜라이더가 버튼 몸체가 닿고 && 버튼이 off
-        if (coll.CompareTag("BUTTON") && !onButton)
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        // 버튼 콜라이더가 오른손의 검지 손가락에 닿고 && 버튼이 off
+        if (coll.collider.CompareTag("RIGHTINDEX") && !onButton)
         {
             buttonCollider.GetComponent<BoxCollider>().enabled = false;     // 콜라이더 비활성화
             Debug.Log("버튼온");
             //onButton = true;                                              // 버튼 on
             audio.PlayOneShot(buttonOn, 1.0f);                              // 버튼 on Sound 재생
-            this.gameObject.GetComponent<Rigidbody>().isKinematic = true;   // configurable joint 무효화
-            transform.localPosition = new Vector3(-0.01f, 0.1039f, 0);           // 위치 고정
+            transform.localPosition = new Vector3(-0.01f, 0.1039f, 0);      // 위치 이동
+            softJointLimit.limit = 0;
+            joint.linearLimit = softJointLimit;
+
             animator.SetBool("isRecord", true);                             // 버튼 깜빡임 애니메이션 on
 
             Invoke("OnButton", 1.0f);
         }
-    }
 
-    void OnCollisionEnter(Collision coll)
-    {
         // 버튼 콜라이더가 오른손의 검지 손가락에 닿고 && 버튼 on
-        if (coll.collider.CompareTag("RIGHTINDEX") && onButton)
+        else if (coll.collider.CompareTag("RIGHTINDEX") && onButton)
         {
             Debug.Log("버튼오프");
             audio.PlayOneShot(buttonOff, 1.0f);                             // 버튼 off 사운드 재생
-            transform.localPosition = new Vector3(-0.0323f, 0.1039f, 0);         // 위치 재고정
+            transform.localPosition = new Vector3(-0.0323f, 0.1039f, 0);    // 위치 이동
             buttonCollider.GetComponent<BoxCollider>().enabled = true;      // 콜라이더 활성화
-            this.gameObject.GetComponent<Rigidbody>().isKinematic = false;  // configurale joint 재 활성화
+            softJointLimit.limit = 0.01f;
+            joint.linearLimit = softJointLimit;
+
             animator.SetBool("isRecord", false);                            // 버튼 깜빡임 애니메이션 off
             Invoke("OffButton", 1.0f);
         }
+
+
     }
 
     void OnButton()

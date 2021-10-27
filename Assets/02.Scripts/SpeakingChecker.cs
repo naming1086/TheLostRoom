de7d15f6b77c8b3eba7ce8c1151ef4a41978ae05
boracles <100pour100@naver.com>
2021-10-28 07:32:33 +0900

    voice particle 형성 코드

--- Assets/02.Scripts/SpeakingChecker.cs
+++ Assets/02.Scripts/SpeakingChecker.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class SpeakingChecker : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public SoundLampButtonCtrl soundLampButtonCtrl;
+    public GameObject voiceParticle;
+
     void Start()
     {
-
+        voiceParticle.SetActive(false);
     }
 
-    void Update()
-    {
-
-    }
-
-    // MainCamera 콜라이더와 닿고 있는 동안 이펙터 형성
+    // 녹음 버튼 on && MainCamera 콜라이더와 닿고 있는 동안 이펙터 형성
     void OnTriggerStay(Collider coll)
     {
-
+        if (soundLampButtonCtrl.onButton && coll.CompareTag("MainCamera"))
+        {
+            voiceParticle.SetActive(true);
+        }
+        else
+        {
+            voiceParticle.SetActive(false);
+        }
     }
 }

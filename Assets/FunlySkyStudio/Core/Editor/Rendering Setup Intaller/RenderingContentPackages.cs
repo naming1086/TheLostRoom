0756eb5ca397b391e47ea9a34f326b2d9c5b2dc8
boracles <100pour100@naver.com>
2021-11-13 17:15:05 +0900

    downloaded asset funly sky studio

---
+++ Assets/FunlySkyStudio/Core/Editor/Rendering Setup Intaller/RenderingContentPackages.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Funly.SkyStudio
+{
+    /// <summary>
+    ///  Name of the latest version of the rendering support content packages.
+    /// </summary>
+    public abstract class RenderingContentPackages
+    {
+        public static string builtinPackage = "Builtin-Content-v2.unitypackage";
+        public static string urpPackage = "URP-Content-v4.unitypackage";
+    }
+}
